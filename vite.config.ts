99ceeae2572dc9853398d239714212e7060fda48
PearAPI <harleyavenue06@gmail.com>
2026-03-22 21:25:49 +0100

    changed gh pages settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/PowerGridDesigner/',
+    base: '/PowerGridDesigner',
     plugins: [react(), tailwindcss()],
 })
